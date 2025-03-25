082ee9fef3ff97f60c437481a433604e1249f5b4
Ojas Kandhare <ojaskandhare@gmail.com>
2025-03-24 18:53:45 -0700

    Update build script to explicitly install ultralytics package

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
+pip install ultralytics
 mkdir -p drivesafe/static/uploads drivesafe/static/detections drivesafe/models/traffic_light drivesafe/templates
 if [ ! -f "drivesafe/models/traffic_light/best_traffic_small_yolo.pt" ]; then
     cp best_traffic_small_yolo.pt drivesafe/models/traffic_light/
